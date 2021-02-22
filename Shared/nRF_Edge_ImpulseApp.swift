074d1066e82563190b79b8b133de179e65827e91
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-02-22 13:57:26 +0000

    New Project

---
+++ Shared/nRF_Edge_ImpulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  nRF_Edge_ImpulseApp.swift
+//  Shared
+//
+//  Created by Dinesh Harjani on 22/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct nRF_Edge_ImpulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
