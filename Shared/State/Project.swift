793fca40e1245940ed83ea10ea29ff54327074c6
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-03-02 12:04:19 +0000

    Custom Decoder for Project Struct
    This custom Decoder allows us to read the Date as a Date, instead of a String. And we can enforce it to always be there, rather than rely on Optionals or Unwrappings and stuff.

--- Shared/State/Project.swift
+++ Shared/State/Project.swift
@@ -13,22 +13,40 @@ struct Project: Identifiable, Codable {
     let id: Int
     let name: String
     let description: String
-    let createdString: String
+    let created: Date
     let owner: String
     
-    // MARK: - Coding Keys
-    
-    enum CodingKeys: String, CodingKey {
-        case createdString = "created"
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.description = try container.decode(String.self, forKey: .description)
+        self.owner = try container.decode(String.self, forKey: .owner)
         
-        case id, name, description, owner
-    }
-    
-    // MARK: - API
-    
-    func createdDate() -> Date? {
+        let createdString = try container.decode(String.self, forKey: .created)
         let formatter = ISO8601DateFormatter()
         formatter.formatOptions =  [.withInternetDateTime, .withFractionalSeconds]
-        return formatter.date(from: createdString)
+        guard let createdDate = formatter.date(from: createdString) else {
+            throw DecodingError.dataCorruptedError(forKey: .created, in: container,
+                          debugDescription: "`Created` Date String does not match expected format.")
+        }
+        self.created = createdDate
     }
+}
+
+// MARK: - Sample Project
+
+extension Project {
+    
+    static let Sample: Project! = try? JSONDecoder().decode(Project.self, from: SampleJSON.data(using: .utf8)!)
+    
+    static let SampleJSON = """
+    {
+        "id": 595,
+        "name": "Sample",
+        "description": "This is just a sample.",
+        "created": "2021-02-26T10:55:47.731Z",
+        "owner": "Pongo Harjani"
+    }
+    """
 }
