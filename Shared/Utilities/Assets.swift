06eddab0dfc856c6617b99680e8f574dcc5d29a2
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-03-03 12:02:31 +0000

    New Assets Utility
    We can 'safely' use our Color Assets using this enum. We can also extend this for images.

---
+++ Shared/Utilities/Assets.swift
@@ -0,0 +1,33 @@
+//
+//  NordicColor.swift
+//  nRF-Edge-Impulse
+//
+//  Created by Dinesh Harjani on 3/3/21.
+//
+
+import SwiftUI
+
+// MARK: - Assets
+
+enum Assets: String {
+    
+    // MARK: - Colors
+    
+    case blue = "NordicBlue"
+    case sky = "NordicSky"
+    case blueslate = "NordicBlueslate"
+    case lake = "NordicLake"
+    
+    case grass = "NordicGrass"
+    case sun = "NordicSun"
+    case red = "NordicRed"
+    case fall = "NordicFall"
+    
+    case lightGrey = "NordicLightGrey"
+    case middleGrey = "NordicMiddleGrey"
+    case darkGrey = "NordicDarkGrey"
+    
+    var color: Color {
+        Color(rawValue)
+    }
+}
