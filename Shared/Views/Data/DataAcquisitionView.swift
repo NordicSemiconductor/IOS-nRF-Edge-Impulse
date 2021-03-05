67a7931262bb2b06e222ff3b180c24b0cba747f2
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-03-05 13:48:52 +0000

    DataAcquisitionView now includes Frequency and Sample Length
    We also changed the Picker Style - I like it more now. But there's some logic remaining, like switching off some Views depending on the selected Sensor.

--- Shared/Views/Data/DataAcquisitionView.swift
+++ Shared/Views/Data/DataAcquisitionView.swift
@@ -10,15 +10,17 @@ import SwiftUI
 struct DataAcquisitionView: View {
     
     private static let Sensors = ["Accelerometer", "Microphone", "Camera"]
+    private static let Frequencies = [8000, 11000, 16000, 32000, 44100, 48000]
     
     let project: Project
     
     @State private var label = ""
     @State private var selectedSensor = 0
+    @State private var sampleLength = 10000
+    @State private var selectedFrequency = 1
     
     var body: some View {
         Form {
-            
             Section(header: Text("Project")) {
                 Text("\(project.name)")
                     .font(.body)
@@ -35,16 +37,40 @@ struct DataAcquisitionView: View {
                         Text(Self.Sensors[i]).tag(i)
                     }
                 }
+                .pickerStyle(InlinePickerStyle())
+                .frame(maxHeight: 75)
             }
             
             Section(header: Text("Sample Length")) {
-                HStack {
-                    Text("ms")
-                    Spacer()
+                Stepper(value: $sampleLength, in: 0...100000) {
+                    Text("\(sampleLength, specifier: "%d") ms")
                 }
             }
+            
+            Section(header: Text("Frequency")) {
+                Picker("Value", selection: $selectedFrequency) {
+                    ForEach(Self.Frequencies.indices) { i in
+                        Text("\(Self.Frequencies[i]) Hz").tag(i)
+                    }
+                }
+                .pickerStyle(InlinePickerStyle())
+                .frame(maxHeight: 75)
+            }
+            
+            Button("Start Sampling") {
+                startSampling()
+            }
+            .accentColor(Assets.red.color)
         }
+        .padding(.top, 8)
         .navigationBarTitle("Data Acquisition")
+    }
+}
+
+private extension DataAcquisitionView {
+    
+    func startSampling() {
+        
     }
 }
 
@@ -53,8 +79,10 @@ struct DataAcquisitionView: View {
 #if DEBUG
 struct NewSampleView_Previews: PreviewProvider {
     static var previews: some View {
-        DataAcquisitionView(project: ProjectList_Previews.previewProjects.first!)
-            .previewDevice("iPhone 12 mini")
+        NavigationView {
+            DataAcquisitionView(project: ProjectList_Previews.previewProjects.first!)
+                .previewDevice("iPhone 12 mini")
+        }
     }
 }
 #endif
