30aafd125de0146168717259defc8b285fc73771
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-03-10 13:25:58 +0000

    Fix for 2-pane LoginView
    On the iPad and the Mac, the LoginView became hidden or lost most of its space.

--- Shared/Views/Native Login/NativeLoginView.swift
+++ Shared/Views/Native Login/NativeLoginView.swift
@@ -32,84 +32,83 @@ struct NativeLoginView: View {
     // MARK: - Body
     
     var body: some View {
-        NavigationView {
-            VStack(alignment: .center) {
-                Spacer()
-                HStack(alignment: .center, spacing: 16) {
-                    Image("Nordic")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 50)
-                    Image("EdgeImpulse")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 80)
-                }
-                HStack(alignment: .lastTextBaseline) {
-                    Image(systemName: "person.fill")
-                        .frame(width: 40, height: 40)
-                        .accentColor(Assets.darkGrey.color)
-                    TextField("Username or E-Mail", text: $username)
-                        .frame(height: 20)
-                        .padding()
-                        .background(textFieldBackground)
-                        .cornerRadius(20)
-//                        .autocapitalization(.none)
-                        .disableAutocorrection(true)
-                        .padding(.bottom, 16)
-                }
-                .padding(.horizontal, 16)
-                HStack(alignment: .lastTextBaseline) {
-                    Image(systemName: "key.fill")
-                        .frame(width: 40, height: 40)
-                        .accentColor(Assets.darkGrey.color)
-                    SecureField("Password", text: $password)
-                        .frame(height: 20)
-                        .padding()
-                        .background(textFieldBackground)
-                        .cornerRadius(20)
-                        .disableAutocorrection(true)
-                        .padding(.bottom, 8)
-                }
-                .padding(.horizontal, 16)
-                
-                if errorMessage.count > 0 {
-                    HStack {
-                        Image(systemName: "info.circle.fill")
-                            .frame(width: 25, height: 25)
-                            .foregroundColor(Assets.red.color)
-                        Text(errorMessage)
-                            .foregroundColor(Assets.red.color)
-                    }
-                    .padding(.bottom, 8)
-                }
-                
-                Link("Forgot your password?", destination: Constant.forgottenPasswordURL)
-                    .foregroundColor(Assets.blue.color)
-                    .padding(.bottom, 8)
-                
-                Button("Login") {
-                    attemptLogin()
-                }
-                .font(.headline)
-                .accentColor(.white)
-                .frame(width: 80, height: 12)
-                .padding()
-                .background(isLoginDisabled ? Assets.lightGrey.color : Assets.blue.color)
-                .cornerRadius(30)
-                .disabled(isLoginDisabled)
-                .padding(.bottom, 8)
-                
-                HStack {
-                    Text("Don't have an account?")
-                        .foregroundColor(Assets.darkGrey.color)
-                    Link("Sign Up", destination: Constant.signupURL)
-                        .foregroundColor(Assets.blue.color)
-                }
-                
-                Spacer()
+        VStack(alignment: .center) {
+            Spacer()
+            HStack(alignment: .center, spacing: 16) {
+                Image("Nordic")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 50)
+                Image("EdgeImpulse")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 80)
             }
-            .navigationTitle("Login")
+            HStack(alignment: .lastTextBaseline) {
+                Image(systemName: "person.fill")
+                    .frame(width: 40, height: 40)
+                    .accentColor(Assets.darkGrey.color)
+                TextField("Username or E-Mail", text: $username)
+                    .frame(maxWidth: 300)
+                    .frame(height: 20)
+                    .padding()
+                    .background(textFieldBackground)
+                    .cornerRadius(20)
+//                        .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .padding(.bottom, 16)
+            }
+            .padding(.horizontal, 16)
+            HStack(alignment: .lastTextBaseline) {
+                Image(systemName: "key.fill")
+                    .frame(width: 40, height: 40)
+                    .accentColor(Assets.darkGrey.color)
+                SecureField("Password", text: $password)
+                    .frame(maxWidth: 300)
+                    .frame(height: 20)
+                    .padding()
+                    .background(textFieldBackground)
+                    .cornerRadius(20)
+                    .disableAutocorrection(true)
+                    .padding(.bottom, 8)
+            }
+            .padding(.horizontal, 16)
+            
+            if errorMessage.count > 0 {
+                HStack {
+                    Image(systemName: "info.circle.fill")
+                        .frame(width: 25, height: 25)
+                        .foregroundColor(Assets.red.color)
+                    Text(errorMessage)
+                        .foregroundColor(Assets.red.color)
+                }
+                .padding(.bottom, 8)
+            }
+            
+            Link("Forgot your password?", destination: Constant.forgottenPasswordURL)
+                .foregroundColor(Assets.blue.color)
+                .padding(.bottom, 8)
+            
+            Button("Login") {
+                attemptLogin()
+            }
+            .font(.headline)
+            .accentColor(.white)
+            .frame(width: 80, height: 12)
+            .padding()
+            .background(isLoginDisabled ? Assets.lightGrey.color : Assets.blue.color)
+            .cornerRadius(30)
+            .disabled(isLoginDisabled)
+            .padding(.bottom, 8)
+            
+            HStack {
+                Text("Don't have an account?")
+                    .foregroundColor(Assets.darkGrey.color)
+                Link("Sign Up", destination: Constant.signupURL)
+                    .foregroundColor(Assets.blue.color)
+            }
+            
+            Spacer()
         }
     }
     
@@ -141,6 +140,7 @@ struct NativeLoginView_Previews: PreviewProvider {
         }
         Group {
             NativeLoginView()
+                .previewDevice("iPad Pro (11-inch) (2nd generation)")
                 .preferredColorScheme(.dark)
         }
     }
