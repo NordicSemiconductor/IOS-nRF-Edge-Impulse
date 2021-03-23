07444a3506328c372b8e905dd49937168e6a5857
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-03-23 12:25:54 +0000

    Snatched CircleImageView from SwiftUILandmarks Tutorial
    It works, so let's just take it away.

---
+++ Shared/Utilities/CircleImageView.swift
@@ -0,0 +1,36 @@
+//
+//  CircleImageView.swift
+//  nRF-Edge-Impulse
+//
+//  Created by Dinesh Harjani on 23/3/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    
+    let image: Image
+    
+    // MARK: - @viewBuilder
+    
+    var body: some View {
+        image
+            .resizable()
+            .frame(width: 120, height: 120, alignment: .leading)
+
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .shadow(radius: 7)
+            .padding()
+    }
+}
+
+// MARK: - Preview
+
+#if DEBUG
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("EdgeImpulse"))
+    }
+}
+#endif
