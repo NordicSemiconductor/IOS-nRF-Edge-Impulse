647157235164f9d1eb1fad72c0216ae5338d7e04
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-03-25 15:55:42 +0000

    Fix for Duplicate Scanned Devices in Scanner / Devices Tab ?
    I think the cause for this is because when we added more fields to the Device struct, it changed how Swift auto-generated the Hash and Equatable. But we consider devices 'equal' if they have the same ID, so I think this should work better.

--- Shared/Bluetooth/Device.swift
+++ Shared/Bluetooth/Device.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// MARK: - RSSI
+
 enum RSSI: Int {
     case outOfRange = 127
     case practicalWorst = -100
@@ -23,12 +25,25 @@ enum RSSI: Int {
         }
     }
 }
+
+// MARK: - Device
     
-struct Device: Identifiable, Hashable {
+struct Device: Identifiable {
     let name: String
     let id: UUID
     let rssi: RSSI
 }
+
+// MARK: - Device.Hashable
+
+extension Device: Hashable {
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+// MARK: - Sample
 
 extension Device {
     static let sample = Device(name: "Test Device", id: UUID(), rssi: .outOfRange)
