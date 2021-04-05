ddf678abf15fbaa6195ede9596419b15fc5dc8cc
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-04-05 11:06:21 +0100

    Project struct now has @collaborators field
    This is the issue we fixed in the previous change, and it cost us hell to figure out why we couldn't decode User(s) properly. It was all because of ourselves. But we spent hours on it on Wednesday, and this morning we fixed it al in ten seconds.

--- Shared/State/Project.swift
+++ Shared/State/Project.swift
@@ -9,12 +9,16 @@ import Foundation
 
 // MARK: - Project
 
-struct Project: Identifiable, Hashable, Codable {
+struct Project: Identifiable, Codable {
+    
     let id: Int
     let name: String
     let description: String
     let created: Date
     let owner: String
+    let collaborators: [User]
+    
+    // MARK: - Init
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
@@ -22,15 +26,23 @@ struct Project: Identifiable, Hashable, Codable {
         self.name = try container.decode(String.self, forKey: .name)
         self.description = try container.decode(String.self, forKey: .description)
         self.owner = try container.decode(String.self, forKey: .owner)
+        self.collaborators = try container.decode([User].self, forKey: .collaborators)
         
         let createdString = try container.decode(String.self, forKey: .created)
-        let formatter = ISO8601DateFormatter()
-        formatter.formatOptions =  [.withInternetDateTime, .withFractionalSeconds]
-        guard let createdDate = formatter.date(from: createdString) else {
+        guard let createdDate = createdString.formatAsDate() else {
             throw DecodingError.dataCorruptedError(forKey: .created, in: container,
                           debugDescription: "`Created` Date String does not match expected format.")
         }
         self.created = createdDate
+    }
+}
+
+// MARK: - Hashable
+
+extension Project: Hashable {
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
     }
 }
 
@@ -46,7 +58,9 @@ extension Project {
         "name": "Sample",
         "description": "This is just a sample.",
         "created": "2021-02-26T10:55:47.731Z",
-        "owner": "Pongo Harjani"
+        "owner": "Pongo Harjani",
+        "collaborators": [
+        ]
     }
     """
 }
