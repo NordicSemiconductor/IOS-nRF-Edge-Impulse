33f6ec4e9393e5e0794f8d8e5e9be676c698ed26
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-04-14 12:21:16 +0100

    PreferencesData is now saved into the Keychain
    This is a lot of duplicated code, I know realise. But at least we have Preferences that have a backing store and are safe.

--- Shared/State/PreferencesData.swift
+++ Shared/State/PreferencesData.swift
@@ -6,9 +6,45 @@
 //
 
 import Foundation
+import KeychainSwift
 
 final class PreferencesData: ObservableObject {
     
-    @Published var onlyScanUARTDevices = true
-    @Published var onlyScanConnectableDevices = true
+    // MARK: - Preferences
+    
+    @Published var onlyScanUARTDevices: Bool {
+        didSet {
+            guard !Constant.isRunningInPreviewMode else { return }
+            keychain.set(onlyScanUARTDevices, forKey: KeychainKeys.onlyScanUARTDevices.rawValue)
+        }
+    }
+    @Published var onlyScanConnectableDevices: Bool {
+        didSet {
+            guard !Constant.isRunningInPreviewMode else { return }
+            keychain.set(onlyScanConnectableDevices, forKey: KeychainKeys.onlyScanConnectableDevices.rawValue)
+        }
+    }
+    
+    // MARK: - Private Properties
+    
+    private var keychain: KeychainSwift
+    
+    // MARK: - Init
+    
+    init() {
+        let keychain = KeychainSwift()
+        
+        self.onlyScanUARTDevices = keychain.getBool(KeychainKeys.onlyScanUARTDevices.rawValue) ?? true
+        self.onlyScanConnectableDevices = keychain.getBool(KeychainKeys.onlyScanConnectableDevices.rawValue) ?? true
+        self.keychain = keychain
+    }
+}
+
+// MARK: - KeychainKeys
+
+private extension PreferencesData {
+    
+    enum KeychainKeys: String, RawRepresentable {
+        case onlyScanUARTDevices, onlyScanConnectableDevices
+    }
 }
