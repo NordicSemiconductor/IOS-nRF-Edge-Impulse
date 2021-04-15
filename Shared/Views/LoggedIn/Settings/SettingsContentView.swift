82983b24aa362129053da55c17c2b4fdfece8483
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-04-15 12:21:52 +0100

    SettingsContentView for iOS is now on par with macOS
    We can now toggle our only two Settings.

--- Shared/Views/LoggedIn/Settings/SettingsContentView.swift
+++ Shared/Views/LoggedIn/Settings/SettingsContentView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct SettingsContentView: View {
+    
+    @EnvironmentObject var preferencesData: PreferencesData
+    
+    // MARK: - View
+    
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            Section(header: Text("Devices")) {
+                Toggle("Only Scan Devices Advertising 'UART' Service",
+                       isOn: $preferencesData.onlyScanUARTDevices)
+                    .toggleStyle(SwitchToggleStyle(tint: Assets.blue.color))
+                
+                Toggle("Only Scan Connectable Devices",
+                       isOn: $preferencesData.onlyScanConnectableDevices)
+                    .toggleStyle(SwitchToggleStyle(tint: Assets.blue.color))
+            }
+            .accentColor(Assets.blue.color)
+        }
     }
 }
 
+// MARK: - Preview
+
+#if DEBUG
 struct SettingsContentView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsContentView()
+            .environmentObject(PreferencesData())
     }
 }
+#endif
