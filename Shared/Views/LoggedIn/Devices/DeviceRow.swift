2ffeeaf2eeb9fc6ba8ed18e4978db76b97f8adef
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-05-14 10:55:04 +0100

    DeviceRow now changes .foregroundColor() depending con Connected / Scanning Status
    This makes the app more interactive.

--- Shared/Views/LoggedIn/Devices/DeviceRow.swift
+++ Shared/Views/LoggedIn/Devices/DeviceRow.swift
@@ -11,6 +11,8 @@ import SwiftUI
 
 struct DeviceRow: View {
     
+    @EnvironmentObject var scannerData: ScannerData
+    
     // MARK: Private Properties
     
     private let device: Device
@@ -26,10 +28,12 @@ struct DeviceRow: View {
     var body: some View {
         HStack(alignment: .top) {
             Image(systemName: "candybarphone")
+                .foregroundColor(deviceForegroundColor)
             
             VStack(alignment: .leading) {
                 Text(device.name)
                     .font(.headline)
+                    .foregroundColor(deviceForegroundColor)
                     .bold()
                 
                 HStack {
@@ -37,10 +41,18 @@ struct DeviceRow: View {
                         .frame(width: 20, height: 15, alignment: .center)
                     
                     Text("\(device.rssi.rawValue) dB")
+                        .foregroundColor(deviceForegroundColor)
                 }
             }
         }
         .padding(8)
+    }
+    
+    var deviceForegroundColor: Color {
+        guard device.state == .notConnected else {
+            return Assets.blue.color
+        }
+        return scannerData.isScanning ? .primary : Assets.middleGrey.color
     }
 }
 
@@ -50,6 +62,7 @@ struct DeviceRow: View {
 struct DeviceRow_Previews: PreviewProvider {
     static var previews: some View {
         DeviceRow(.sample)
+            .environmentObject(Preview.mockScannerData)
             .previewLayout(.sizeThatFits)
     }
 }
