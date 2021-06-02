a997892b3067181a65ab60e1250032d81f5ccbd4
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-06-02 10:23:14 +0100

    Small helper function to decode JSON Samples
    Instead of repeating the same code again and again, we just call the same new function and stop worrying about it.

--- Shared/Utilities/Previews.swift
+++ Shared/Utilities/Previews.swift
@@ -15,24 +15,15 @@ struct Preview {
     static var previewUser = User(id: 3, username: "independence.day", name: "ID4", created: Date())
     
     static var previewProjects: [Project]! = {
-        let path: String! = Bundle.main.path(forResource: "sample_projects", ofType: "json")
-        let content: String! = try? String(contentsOfFile: path)
-        let contentData: Data! = content.data(using: .utf8)
-        return try? JSONDecoder().decode([Project].self, from: contentData)
+        return try? decode(filename: "sample_projects")
     }()
     
     static let previewDataSamples: [DataSample]! = {
-        let path: String! = Bundle.main.path(forResource: "sample_datasamples", ofType: "json")
-        let content: String! = try? String(contentsOfFile: path)
-        let contentData: Data! = content.data(using: .utf8)
-        return try? JSONDecoder().decode([DataSample].self, from: contentData)
+        return try? decode(filename: "sample_datasamples")
     }()
     
     static let previewFullDataSampleResponse: FullDataAcquisitionData! = {
-        let path: String! = Bundle.main.path(forResource: "sample_full_microphone_data_response", ofType: "json")
-        let content: String! = try? String(contentsOfFile: path)
-        let contentData: Data! = content.data(using: .utf8)
-        return try? JSONDecoder().decode(FullDataAcquisitionData.self, from: contentData)
+        return try? decode(filename: "sample_full_microphone_data_response")
     }()
     
     static let projectsPreviewAppData = previewAppData(.complete(previewUser, previewProjects))
@@ -83,5 +74,17 @@ struct Preview {
         ]
         return scannerData
     }()
+}
+
+// MARK: - Preview
+
+fileprivate extension Preview {
+    
+    static func decode<T: Decodable>(filename: String) throws -> T? {
+        let path: String! = Bundle.main.path(forResource: filename, ofType: "json")
+        let content: String! = try? String(contentsOfFile: path)
+        let contentData: Data! = content.data(using: .utf8)
+        return try? JSONDecoder().decode(T.self, from: contentData)
+    }
 }
 #endif
