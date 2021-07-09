23983aa1d3bd7813fd933b7da1606db52708a16e
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-07-09 10:16:47 +0100

    SocketIOJobMessage now filters \n
    Those are horrible. Thanks, Edge Impulse.

--- Shared/Network/WebSockets/SocketIOJobMessage.swift
+++ Shared/Network/WebSockets/SocketIOJobMessage.swift
@@ -12,15 +12,18 @@ struct SocketIOJobMessage {
     let kind: String
     let message: String
     
-    init(from dataString: String) throws {
-        let pattern = #"[0-9]+\["job-(.*)-[0-9]+",\{"data":"(.*)[\\]n*"\}\]"#
-        let regex = try NSRegularExpression(pattern: pattern, options: [])
-        let nsrange = NSRange(dataString.startIndex..<dataString.endIndex, in: dataString)
-        guard let match = regex.firstMatch(in: dataString, options: [], range: nsrange),
+    init(from inputString: String) throws {
+        let cleanString = inputString.replacingOccurrences(of: "\\n", with: "")
+        
+        // Use https://regexr.com/ to check RegExes.
+        let pattern = #"[0-9]+\["job-(.*)-[0-9]+",\{"data":"(.*)"\}\]"#
+        let regEx = try NSRegularExpression(pattern: pattern, options: [])
+        let nsrange = NSRange(cleanString.startIndex..<cleanString.endIndex, in: cleanString)
+        guard let match = regEx.firstMatch(in: cleanString, options: [], range: nsrange),
               // +1 because the full string is returned as the first match
               match.numberOfRanges == 3 else { throw NordicError.testError }
         
-        kind = String(dataString[Range(match.range(at: 1), in: dataString)!])
-        message = String(dataString[Range(match.range(at: 2), in: dataString)!])
+        kind = String(cleanString[Range(match.range(at: 1), in: cleanString)!])
+        message = String(cleanString[Range(match.range(at: 2), in: cleanString)!])
     }
 }
