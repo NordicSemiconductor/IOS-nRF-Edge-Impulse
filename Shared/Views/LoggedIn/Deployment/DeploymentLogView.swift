54978fe7d78aaca953995ae182a3a6a9feab273f
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-07-12 11:04:15 +0100

    Spun off DeploymentLogView into its own file
    So now, the current UI of DeploymentView is a lot cleaner and better presented now.

---
+++ Shared/Views/LoggedIn/Deployment/DeploymentLogView.swift
@@ -0,0 +1,47 @@
+//
+//  DeploymentLogView.swift
+//  nRF-Edge-Impulse
+//
+//  Created by Dinesh Harjani on 12/7/21.
+//
+
+import SwiftUI
+
+struct DeploymentLogView: View {
+    
+    @EnvironmentObject var viewState: DeploymentViewState
+    
+    var body: some View {
+        List {
+            ForEach(viewState.jobMessages) { message in
+                Text(message.message)
+            }
+        }
+        .introspectTableView { tableView in
+            viewState.jobMessages.publisher
+                .debounce(for: 50, scheduler: DispatchQueue.main)
+                .collect()
+                .sink { [weak tableView] _ in
+                    guard let tableView = tableView, let dataSource = tableView.dataSource,
+                          let sections = dataSource.numberOfSections?(in: tableView), sections > 0 else { return }
+                    let indexPath = IndexPath(row: viewState.jobMessages.count - 1, section: 0)
+                    tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+                }
+                .store(in: &viewState.cancellables)
+        }
+    }
+}
+
+// MARK: - Preview
+
+#if DEBUG
+struct DeploymentLogView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            DeploymentLogView()
+                .environmentObject(DeploymentViewState())
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}
+#endif
--- Shared/Views/LoggedIn/Deployment/DeploymentView.swift
+++ Shared/Views/LoggedIn/Deployment/DeploymentView.swift
@@ -22,23 +22,9 @@ struct DeploymentView: View {
         VStack {
             switch viewState.status {
                 case .buildingModel(_), .error(_):
-                    List {
-                        ForEach(viewState.jobMessages) { message in
-                            Text(message.message)
-                        }
-                    }
-                    .introspectTableView { tableView in
-                        viewState.jobMessages.publisher
-                            .debounce(for: 50, scheduler: DispatchQueue.main)
-                            .collect()
-                            .sink { [weak tableView] _ in
-                                guard let tableView = tableView, let dataSource = tableView.dataSource, let sections = dataSource.numberOfSections?(in: tableView), sections > 0 else { return }
-                                let indexPath = IndexPath(row: viewState.jobMessages.count - 1, section: 0)
-                                tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
-                            }
-                            .store(in: &viewState.cancellables)
-                    }
-                    .padding(.top)
+                    DeploymentLogView()
+                        .environmentObject(viewState)
+                        .padding(.top)
                 default:
                     DeploymentConfigurationView()
                         .environmentObject(viewState)
