a41aca525a0eb36d97cb8b736f9ad62e1ad389e7
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-07-13 15:53:49 +0100

    Auto-Scroll of Deployment Logs now works much better
    Whereas before it was not really working, I'll admit.

---
+++ Shared/Utilities/Extensions/Introspect.swift
@@ -0,0 +1,25 @@
+//
+//  Introspect.swift
+//  nRF-Edge-Impulse
+//
+//  Created by Dinesh Harjani on 13/7/21.
+//
+
+import Foundation
+
+#if canImport(UIKit)
+import UIKit
+
+extension UITableView {
+    
+    func scrollToBottom() {
+        guard let dataSource = dataSource, let numberOfSections = dataSource.numberOfSections?(in: self),
+              numberOfSections > 0 else { return }
+        let numberOfRows = dataSource.tableView(self, numberOfRowsInSection: numberOfSections - 1)
+        guard numberOfRows > 0 else { return }
+        let indexPath = IndexPath(row: numberOfRows - 1, section: numberOfSections - 1)
+        scrollToRow(at: indexPath, at: .bottom, animated: false)
+    }
+}
+
+#endif
--- Shared/Views/LoggedIn/Deployment/DeploymentLogView.swift
+++ Shared/Views/LoggedIn/Deployment/DeploymentLogView.swift
@@ -18,17 +18,10 @@ struct DeploymentLogView: View {
             }
         }
         .introspectTableView { tableView in
-            viewState.logMessages.publisher
-                .debounce(for: 300, scheduler: DispatchQueue.main)
-                .collect()
+            viewState.$logMessages
+                .throttle(for: .seconds(2), scheduler: RunLoop.main, latest: true)
                 .sink { [weak tableView] _ in
-                    guard let tableView = tableView, let dataSource = tableView.dataSource,
-                          let sections = dataSource.numberOfSections?(in: tableView),
-                          sections > 0 else { return }
-                    let numberOfRows = dataSource.tableView(tableView, numberOfRowsInSection: 0)
-                    guard numberOfRows > 0 else { return }
-                    let indexPath = IndexPath(row: numberOfRows - 1, section: 0)
-                    tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+                    tableView?.scrollToBottom()
                 }
                 .store(in: &viewState.cancellables)
         }
