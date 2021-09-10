21fb8aa13200b8ad548c5ca969130a9959c03a7b
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-09-10 10:03:02 +0100

    Sample Length can now be set using the keyboard
    This is for both iOS and macOS. I think it's not perfect UI-wise, but the binding works reasonably well. On macOS it's slightly better.

--- Shared/Views/LoggedIn/Data/DataAcquisitionViewSampleLengthPicker.swift
+++ Shared/Views/LoggedIn/Data/DataAcquisitionViewSampleLengthPicker.swift
@@ -16,10 +16,40 @@ struct DataAcquisitionViewSampleLengthPicker: View {
             if let sensor = viewState.selectedSensor,
                sensor != Constant.unselectedSensor,
                let maxSampleLength = viewState.selectedSensor.maxSampleLengthS {
-                HStack {
-                    Slider(value: $viewState.sampleLengthS, in: 0...Double(maxSampleLength))
-                        .accentColor(.universalAccentColor)
-                    Text("\(Int(viewState.sampleLengthS)) seconds")
+                
+                VStack {
+                    HStack {
+                        Slider(value: $viewState.sampleLengthS, in: 0...Double(maxSampleLength))
+                            .accentColor(.universalAccentColor)
+                        Text("\(Int(viewState.sampleLengthS)) seconds")
+                    }
+                    
+                    #if os(iOS)
+                    Divider()
+                    #endif
+                    
+                    HStack {
+                        #if os(iOS)
+                        Text("Text Input")
+                            .foregroundColor(.disabledTextColor)
+                            .frame(height: 40)
+                        Spacer()
+                        #endif
+                        
+                        TextField("AA", text: Binding(
+                            get: {
+                                String(format: "%.0f", viewState.sampleLengthS)
+                            },
+                            set: {
+                                let sensor = viewState.selectedSensor
+                                guard sensor != Constant.unselectedSensor else { return }
+
+                                let maxSampleLengthS = Double(sensor.maxSampleLengthS ?? .max)
+                                viewState.sampleLengthS = min(Double($0) ?? 0, maxSampleLengthS)
+                            }
+                        ))
+                        //                .keyboardType(.numberPad)
+                    }
                 }
             } else {
                 Text("Unavailable")
