4e2c06979e200f9a6683a7664ed84135aac4439b
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-09-22 13:59:36 +0100

    DFUManifest Update
    The file has changed since we last 'touched' it.

--- Shared/Bluetooth/DFU/DFUManifest.swift
+++ Shared/Bluetooth/DFU/DFUManifest.swift
@@ -26,21 +26,43 @@ struct DFUManifest: Codable {
 extension DFUManifest {
     
     struct File: Codable {
+        
+        let imageIndex: Int
         let size: Int
         let file: String
-        let modTime: TimeInterval
-        let loadAddress: Int
-        let mcuBootVersion: String
+        let modTime: Int
+        let mcuBootVersion: String?
         let type: String
         let board: String
         let soc: String
         
+        // MARK: CodingKeys
+        
         enum CodingKeys: String, CodingKey {
             case size, file
             case modTime = "modtime"
-            case loadAddress = "load_address"
             case mcuBootVersion = "version_MCUBOOT"
             case type, board, soc
+            case imageIndex = "image_index"
+        }
+        
+        // MARK: Init
+        
+        init(from decoder: Decoder) throws {
+            let values = try decoder.container(keyedBy: CodingKeys.self)
+            size = try values.decode(Int.self, forKey: .size)
+            file = try values.decode(String.self, forKey: .file)
+            modTime = try values.decode(Int.self, forKey: .modTime)
+            mcuBootVersion = try? values.decode(String.self, forKey: .mcuBootVersion)
+            type = try values.decode(String.self, forKey: .type)
+            board = try values.decode(String.self, forKey: .board)
+            soc = try values.decode(String.self, forKey: .soc)
+            let imageIndexString = try values.decode(String.self, forKey: .imageIndex)
+            guard let imageIndex = Int(imageIndexString) else {
+                throw DecodingError.dataCorruptedError(forKey: .imageIndex, in: values,
+                                                       debugDescription: "`imageIndex` could not be parsed from String to Int.")
+            }
+            self.imageIndex = imageIndex
         }
     }
 }
