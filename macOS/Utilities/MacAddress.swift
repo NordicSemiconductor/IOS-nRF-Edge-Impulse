39e3c73645ac9a6828b2ed5ff9b31f31906057a6
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-11-17 15:41:49 +0000

    Fix: the Mac has its own independent framework for Bluetooth, IOBluetooth
    So we don't need to pipe tasks into Unix to find out our MAC Address, we can just ask the framework!

--- macOS/Utilities/MacAddress.swift
+++ macOS/Utilities/MacAddress.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import IOBluetooth
 
 final class MacAddress {
     
@@ -13,50 +14,17 @@ final class MacAddress {
     
     // MARK: - Private
     
-    private let lock: NSLock
-    private var _value: String?
+    private let controller: IOBluetoothHostController
+    private var address: String?
     
     private init() {
-        self.lock = NSLock()
-        self._value = nil
+        self.controller = IOBluetoothHostController.default()
     }
     
     func get() -> String? {
-        // TODO: Fix for macOS 12 or remove.
-        if #available(macOS 12.0, *) {
-            return nil // App Sandbox doesn't allow us to call system_profiler.
-        }
-        
-        // Ensure Exclusive Access. Or attempt to, at least - the previous way we had to
-        // launch the system_profiler process crashed the app randomly with 'Simultaneous
-        // access' error.
-        lock.lock()
-        defer {
-            lock.unlock()
-        }
-        
-        guard _value == nil else { return _value }
-        
-        let task = Process()
-        task.executableURL = URL(fileURLWithPath: "/usr/sbin/system_profiler")
-        task.arguments = ["SPBluetoothDataType"]
-        
-        let pipe = Pipe()
-        task.standardOutput = pipe
-        task.standardError = pipe
-        
-        do {
-            try task.run()
-            
-            let data = pipe.fileHandleForReading.readDataToEndOfFile()
-            let lines = String(data: data, encoding: .utf8)?.split(separator: "\n")
-            guard let addressLine = lines?.first(where: { $0.contains("Address:") }) else { return nil }
-            var result = addressLine.trimmingCharacters(in: .whitespacesAndNewlines)
-            result.removeFirst(9)
-            _value = String(result)
-            return _value
-        } catch {
-            return nil
-        }
+        guard address == nil else { return address }
+
+        address = controller.addressAsString().uppercased()
+        return address
     }
 }
