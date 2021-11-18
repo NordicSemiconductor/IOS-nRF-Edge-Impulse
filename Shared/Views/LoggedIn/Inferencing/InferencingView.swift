77ae89dfb42e36e3004848c7705eb13a519d2347
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2021-11-18 11:11:41 +0000

    Refactor: InferencingResultRow is now on its own file
    I think this is better. Less clutter.

--- Shared/Views/LoggedIn/Inferencing/InferencingView.swift
+++ Shared/Views/LoggedIn/Inferencing/InferencingView.swift
@@ -110,40 +110,6 @@ struct InferencingResultsHeaderRow: View {
     }
 }
 
-// MARK: - InferencingResultRow
-
-struct InferencingResultRow: View {
-    
-    static let classificationValueFormatter: NumberFormatter = {
-        let formatter = NumberFormatter()
-        formatter.roundingMode = .halfUp
-        formatter.minimumFractionDigits = 4
-        formatter.maximumFractionDigits = 4
-        return formatter
-    }()
-    
-    let result: InferencingResults
-    private let gridItems: [GridItem]
-    
-    init(_ result: InferencingResults) {
-        self.result = result
-        self.gridItems = Array(repeating: InferencingView.CellType,
-                               count: result.classification.count)
-    }
-    
-    var body: some View {
-        LazyVGrid(columns: gridItems) {
-            ForEach(result.classification, id: \.self) { classification in
-                Text(InferencingResultRow.classificationValueFormatter.string(from: classification.value as NSNumber) ?? "N/A")
-                    .foregroundColor(classification.value > 0.6
-                                     ? .green : .gray)
-                    .font(.system(.body, design: .monospaced))
-                    .fontWeight(.light)
-            }
-        }
-    }
-}
-
 // MARK: - Preview
 
 #if DEBUG
