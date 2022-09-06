098762e97cb363993cb4c9e015a5be08c9f2fefb
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2022-09-06 12:49:45 +0100

    Adopted AppIconView from iOS-Common-Libraries instead
    I think we moved it there, but then didn't remember to come back and use it from here.

---
+++ Shared/Views/Utilities/SmallAppIconAndVersionView.swift
@@ -0,0 +1,35 @@
+//
+//  AppIconView.swift
+//  nRF-Edge-Impulse
+//
+//  Created by Dinesh Harjani on 1/7/21.
+//
+
+import SwiftUI
+import iOS_Common_Libraries
+
+// MARK: - SmallAppIconAndVersionView
+
+struct SmallAppIconAndVersionView: View {
+    
+    var body: some View {
+        HStack {
+            AppIconView()
+                .saturation(0.3)
+                .frame(width: 16, height: 16)
+            Text(Constant.appVersion)
+                .font(.caption)
+        }
+    }
+}
+
+// MARK: - Preview
+
+struct AppIconView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SmallAppIconAndVersionView()
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}
